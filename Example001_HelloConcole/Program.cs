d3df74e517974639ee7d6537e91555436427c86e
Andrey Salikov <79645878868@ya.ru>
2022-09-28 20:20:06 +0300

    init commit

---
+++ Example001_HelloConcole/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// To add new coment need clik ctrl + /
+// dotnet new console
+// dot net run
+// 
+Console.WriteLine("1 Hello, World!");
+Console.WriteLine("2 Hello, World!");
+Console.WriteLine("3 Hello, World!");
