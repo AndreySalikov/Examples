6d88e07cabbeb2ebb5c4b5bd5630f87fec5fa089
Andrey Salikov <79645878868@ya.ru>
2022-09-29 14:11:40 +0300

    Поиск числа

---
+++ Example019_MethodsGuide0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+int ii = new Random().Next(1,100);
+
+int user;
+
+do
+{
+    Console.Write("N: ");
+    int.TryParse(Console.ReadLine(), out user);
+    if (user > ii) Console.WriteLine("user > ii");
+    if (user < ii) Console.WriteLine("user < ii");
+}
+while (user != ii);
+Console.WriteLine("Красавчик");
+Console.Write(ii);
