c45ceabb98750584d27fc0a8df8bb3f833751a3e
Andrey Salikov <79645878868@ya.ru>
2022-09-29 12:36:57 +0300

    Changed data type int to double

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,20 @@
-﻿double numberA = 8;
-double numberB = 2;
-double result = numberA / numberB;
+﻿// Деление в целых числах
+int numberA = 12;
+int numberB = 5;
+Console.WriteLine (numberA /numberB);
+// Деление в целых числах
+int numberAA = new Random().Next(5, 100);
+int numberBB = new Random().Next(20, 100);
+Console.WriteLine (numberAA /numberBB);
+
+// Деление с остатоком
+double numberC = 12;
+double numberD = 5;
+double result = numberC / numberD;
 Console.WriteLine (result);
+
+// Деление с остатоком
+double numberCC = new Random().Next(5, 100);
+double numberDD = new Random().Next(20, 100);
+double result_new = numberCC / numberDD;
+Console.WriteLine (result_new);
