2365c5271e07ed9b66d732d7a7543dde1d7db804
Andrey Salikov <79645878868@ya.ru>
2022-10-12 03:10:28 +0300

    Добавил новые примеры

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,30 +1,30 @@
-﻿// void FillArray(int[] collection)
-// {
-//     int length = collection.Length;
-//     int index = 0;
-//     while (index < length)
-//     {
-//         collection[index] = new Random().Next(1, 10);
-//         index++;
-//     }
-// }
-void FillArray(int[] collection)
+﻿void FillArray(int[] collection)
 {
     int length = collection.Length;
     int index = 0;
     while (index < length)
     {
-        collection[index] = Convert.ToInt32(Console.ReadLine());
+        collection[index] = new Random().Next(1, 10);
         index++;
     }
 }
+// void FillArray(int[] collection)
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = Convert.ToInt32(Console.ReadLine());
+//         index++;
+//     }
+// }
 void PrintArray(int[] col)
 {
     int count = col.Length;
     int position = 0;
     while (position < count)
     {
-        System.Console.WriteLine(col[position]);
+        System.Console.Write(col[position] + " ");
         position++;
     }
 }
@@ -48,12 +48,14 @@ int IndexOf(int[] collection, int find)
 int[] array = new int[10]; // создаем массив длиной 10 чисел по умалчанию он заполниться нулями
 System.Console.WriteLine("Введи масив");
 FillArray(array);
-array[4] = 4;
-array[6] = 4;
+// array[4] = 4;
+// array[6] = 4;
 System.Console.WriteLine("Твой М");
 PrintArray(array);
 System.Console.WriteLine();
 
-int pos = IndexOf(array, 5);
-System.Console.Write("Индекс");
+int pos = IndexOf(array, 4);
+System.Console.Write("Индекс ");
 System.Console.WriteLine(pos);
+
+
---
+++ Example020/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.Clear();
+
+// using System;
+// class PleaseSayUra
+// {
+//     static void Main()
+//     {
+//         System.Console.WriteLine("Ura");
+//         Console.ReadLine();
+//     }
+// }
+
+int count = 0;
+while (count < 10)
+{
+    System.Console.Write(count + " ");
+    count++;
+}
+
+System.Console.WriteLine();
+
+int number = Convert.ToInt32(new Random().Next(-1,1));
+System.Console.WriteLine(number);
+if (number == 0)
+{
+    System.Console.WriteLine("TRUE");
+}
+else
+{
+    System.Console.WriteLine("FALSE");
+}
+
+for(int i = 0; i < 10; i++)
+{
+    System.Console.Write(-i+" ");
+}
+
+System.Console.WriteLine();
+System.Console.WriteLine("Методы записи в масив и печати массва");
+
+void FillArray (int[] collection)
+{
+    int length = collection.Length; 
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = Convert.ToInt32(new Random(). Next(99, 199));
+        index = index +1; // index++
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        System.Console.Write(collection[index] + ", ");
+        index++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
