5de5b9d2f804b9091b8e7bce3b5b0c6c9ac91285
Andrey Salikov <79645878868@ya.ru>
2022-10-17 05:00:32 +0300

    added example021

---
+++ Example021/Program.cs
@@ -0,0 +1,13 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class PleaseSayUra
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Ura");
+        Console.ReadLine();
+    }
+}
